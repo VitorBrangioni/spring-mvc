d781ac242593c5fecfa634da11a287aae7b9649f
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-03-18 15:34:29 -0300

    Created Controller for insert and show the data from database

--- src/br/com/spring/mvc/vitorbrangioni/controller/IndexController.java
+++ src/br/com/spring/mvc/vitorbrangioni/controller/IndexController.java
@@ -1,14 +1,37 @@
 package br.com.spring.mvc.vitorbrangioni.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.com.spring.mvc.vitorbrangioni.model.User;
+import br.com.spring.mvc.vitorbrangioni.model.dao.UserDAO;
 
 @Controller
 public class IndexController {
 	
-	@RequestMapping("/helloWorld")
-	public String start() {
+	@RequestMapping("/register-post")
+	public String start(Model model, User user) {
+		
+		UserDAO userDAO = new UserDAO();
+		userDAO.insert(user);
+		
+		model.addAttribute("name", user.getName());
 		return "index";
+	}
+	
+	@RequestMapping("/register")
+	public String register() {
+		return "register";
+	}
+	
+	@RequestMapping("/users")
+	public String showAll(Model model) {
+		UserDAO userDAO = new UserDAO();
+		model.addAttribute("users", userDAO.show());
+		
+		return "users";
 	}
 
 }
