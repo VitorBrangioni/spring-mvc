099916ab9e0dd902e24dc768ec075752b0bca504
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-03-18 15:36:21 -0300

    Created User pojo

---
+++ src/br/com/spring/mvc/vitorbrangioni/model/User.java
@@ -0,0 +1,46 @@
+package br.com.spring.mvc.vitorbrangioni.model;
+
+public class User {
+	
+	public String name;
+	private String familyName;
+	private String cpf;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the familyName
+	 */
+	public String getFamilyName() {
+		return familyName;
+	}
+	/**
+	 * @param familyName the familyName to set
+	 */
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+	/**
+	 * @return the cpf
+	 */
+	public String getCpf() {
+		return cpf;
+	}
+	/**
+	 * @param cpf the cpf to set
+	 */
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+}
